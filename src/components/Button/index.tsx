1eb6ccd254a8e1535e49fd86db87f12c3c691ee6
jinpill <hello@jinpill.dev>
2024-01-05 08:32:32 +0900

    feat: Update stories of Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,30 +2,60 @@ import React from "react";
 import classNames from "classnames";
 import style from "./style.module.scss";
 
-type ButtonProps = {
-  type?: "button" | "anchor";
-  role?: "primary" | "secondary" | "tertiary" | "danger";
+export type ButtonRole = "primary" | "secondary" | "tertiary" | "danger";
+export type ButtonSize = "small" | "medium" | "large";
+
+export type ButtonProps = {
+  role?: ButtonRole;
+  size?: ButtonSize;
   text?: string;
+  href?: string;
   isDisabled?: boolean;
+
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onPointerEnter?: (event: React.PointerEvent<HTMLButtonElement>) => void;
+  onPointerLeave?: (event: React.PointerEvent<HTMLButtonElement>) => void;
+  onPointerDown?: (event: React.PointerEvent<HTMLButtonElement>) => void;
+  onPointerUp?: (event: React.PointerEvent<HTMLButtonElement>) => void;
 
   className?: string;
   children?: React.ReactNode;
 };
 
-const Button = (props: ButtonProps) => {
-  return (
-    <button
-      className={classNames(
-        style.button,
-        props.className,
-        style[props.role ?? "primary"],
-      )}
-      disabled={props.isDisabled}
-    >
-      {props.children}
-      {props.text}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (props.href) {
+        window.location.href = props.href;
+      }
 
+      if (props.onClick) {
+        props.onClick(event);
+      }
+    };
+
+    return (
+      <button
+        ref={ref}
+        className={classNames(
+          style.button,
+          props.className,
+          style[props.role ?? "primary"],
+          style[props.size ?? "medium"],
+        )}
+        disabled={props.isDisabled}
+        onClick={handleClick}
+        onPointerEnter={props.onPointerEnter}
+        onPointerLeave={props.onPointerLeave}
+        onPointerDown={props.onPointerDown}
+        onPointerUp={props.onPointerUp}
+      >
+        {props.children}
+        {props.text}
+      </button>
+    );
+  },
+);
+
+Button.displayName = "Button";
 export default Button;
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,50 +1,41 @@
+import Button from "../components/Button";
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "./Button";
-
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Button> = {
   title: "Example/Button",
   component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  parameters: {},
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  argTypes: {},
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    text: "Primary",
+    role: "primary",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: "Button",
+    text: "Secondary",
+    role: "secondary",
   },
 };
 
-export const Large: Story = {
+export const Tertiary: Story = {
   args: {
-    size: "large",
-    label: "Button",
+    text: "Tertiary",
+    role: "tertiary",
   },
 };
 
-export const Small: Story = {
+export const Danger: Story = {
   args: {
-    size: "small",
-    label: "Button",
+    text: "Danger",
+    role: "danger",
   },
 };
