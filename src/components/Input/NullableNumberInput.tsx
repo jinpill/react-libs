34c9e731dba0e5505c871fb5d363499abd8d5420
jinpill <jinlee@metamorp.cc>
2024-05-28 21:30:49 +0900

    Added the Input.NullableNumber component

---
+++ src/components/Input/NullableNumberInput.tsx
@@ -0,0 +1,37 @@
+import React, { useMemo } from "react";
+import BaseInput, { BaseInputProps } from "./BaseInput";
+
+export type NullableNumberInputProps = {
+  value?: number | null;
+  onChange?: (value: number | null) => void;
+} & Omit<BaseInputProps, "type" | "value" | "onChange">;
+
+const NullableNumberInput = React.forwardRef<
+  HTMLDivElement,
+  NullableNumberInputProps
+>((props, ref) => {
+  const { value: _value, onChange, ...restProps } = props;
+
+  const value = useMemo(() => {
+    if (_value === null) return "";
+    if (typeof _value === "number") return _value.toString();
+    return _value;
+  }, [_value]);
+
+  const handleChange = (value: string) => {
+    const nextValue = value === "" ? null : Number(value);
+    onChange?.(nextValue);
+  };
+
+  return (
+    <BaseInput
+      ref={ref}
+      type="number"
+      value={value}
+      onChange={handleChange}
+      {...restProps}
+    />
+  );
+});
+
+export default NullableNumberInput;
--- src/components/Input/NumberInput.tsx
+++ src/components/Input/NumberInput.tsx
@@ -1,30 +1,23 @@
-import React, { useMemo } from "react";
-import BaseInput, { BaseInputProps } from "./BaseInput";
+import React from "react";
+import { BaseInputProps } from "./BaseInput";
+import NullableNumberInput from "./NullableNumberInput";
 
 export type NumberInputProps = {
-  value?: number | null;
-  onChange?: (value: number | null) => void;
+  value?: number;
+  onChange?: (value: number) => void;
 } & Omit<BaseInputProps, "type" | "value" | "onChange">;
 
 const NumberInput = React.forwardRef<HTMLDivElement, NumberInputProps>(
   (props, ref) => {
-    const { value: _value, onChange, ...restProps } = props;
+    const { value, onChange, ...restProps } = props;
 
-    const value = useMemo(() => {
-      if (_value === null) return "";
-      if (typeof _value === "number") return _value.toString();
-      return _value;
-    }, [_value]);
-
-    const handleChange = (value: string) => {
-      const nextValue = value === "" ? null : Number(value);
-      onChange?.(nextValue);
+    const handleChange = (value: number | null) => {
+      onChange?.(value ?? 0);
     };
 
     return (
-      <BaseInput
+      <NullableNumberInput
         ref={ref}
-        type="number"
         value={value}
         onChange={handleChange}
         {...restProps}
